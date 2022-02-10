53a68f0c4cb7d6afe2eaa3736a42cadd7e269e61
Jdimelow <92814704+Jdimelow@users.noreply.github.com>
2022-02-10 15:05:45 -0600

    Add files via upload

--- AudioHarmonizerSerial.ino
+++ AudioHarmonizerSerial.ino
@@ -17,18 +17,20 @@
 
 BluetoothSerial SerialBT;
 
+
+
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32Test"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
-void loop() {
+void loop() { 
   if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
+    
   if (SerialBT.available()) {
-    //Serial.write(SerialBT.read());
     //Serial.println(SerialBT.readString());
     //Serial.println("");
     
@@ -41,19 +43,18 @@ void loop() {
     char *str;
     int index=0;
     String introInput[4];
-    while (index < 4 && (str = strtok_r(p, ".", &p)) != NULL ){
+    while (index < 4 && (str = strtok_r(p, ";", &p)) != NULL ){
       introInput[index]=str;
       index++;
       //Serial.println(str);
     }
     Serial.println(p);
-    if(introInput[3] == "Automatic")
-    {
+    if(introInput[3] == "Automatic"){
       Serial.println("In automatic mode");
       String autoInput[5];
       int autoIndex = 0;
       char *str;
-      while ((str = strtok_r(p, ".", &p)) != NULL){
+      while ((str = strtok_r(p, ";", &p)) != NULL){
         autoInput[autoIndex]=str;
         autoIndex++;
       }
@@ -62,31 +63,92 @@ void loop() {
         Serial.println(autoInput[i]);
       }
       //Serial.println(str); 
-    }
-    else
-    {
+    } 
+    else{
       Serial.println("In manual mode");
-      String mlength = String(strtok_r(p, ".", &p));
-      String harmonies = String(strtok_r(p, ".", &p));
+      String mlength = String(strtok_r(p, ";", &p));
+      String harmonies = String(strtok_r(p, ";", &p));
       Serial.println(harmonies);
-      String noteNumber = String(strtok_r(p, ".", &p));
+      String noteNumber = String(strtok_r(p, ";", &p));
       String manualInput[mlength.toInt()];
       int manualIndex=0;
       char *str;
       Serial.println("about to parse");
-      while ((str = strtok_r(p, ".", &p)) != NULL){
+      while ((str = strtok_r(p, ";", &p)) != NULL){
         manualInput[manualIndex]=str;
         manualIndex++;
-        Serial.println(manualIndex);
+        //Serial.println(manualIndex);
       }
 
       
-//      for(int i=0;i<mlength.toInt();i++)
-//      {
-//        Serial.println(manualInput[i]);
-//      }
+      for(int i=0;i<mlength.toInt();i++)
+      {
+        Serial.println(manualInput[i]);
+      }
     }
      
   }
-  delay(20);
+  
+  int batteryLevel = 57;
+  int oldBatteryLevel = 0;
+  for(int i = 0; i < 7; i++) {
+    oldBatteryLevel = batteryLevel;
+    batteryLevel = random(0, 100);
+    batteryMonitor(batteryLevel, oldBatteryLevel);
+    //Serial.println(oldBatteryLevel);
+    Serial.println(batteryLevel);
+  }
+  
+  
+  
+  delay(5000);
+}
+
+ void batteryMonitor(int batteryLevel, int oldBatteryLEvel) {
+   if(batteryLevel != oldBatteryLEvel){
+    int splitArray[3] = {0, 0, 0};
+    splitInteger(splitArray, batteryLevel);
+    convertAscii(splitArray);
+  }
+}
+
+void splitInteger(int splitArray[3], int batteryLevel){
+  for(int i=2; i>=0; i--){
+    splitArray[i] = (batteryLevel%10);
+    batteryLevel /= 10;
+  }
+}
+
+void convertAscii(int splitArray[3]){
+  int batteryLevel[3];
+  // convert to decimal
+  for(int i=0; i<3; i++){
+    if(splitArray[i] == 0){
+      batteryLevel[i] = 48;
+    }else if(splitArray[i] == 1){
+      batteryLevel[i] = 49;
+    }else if(splitArray[i] == 2){
+      batteryLevel[i] = 50;
+    }else if(splitArray[i] == 3){
+      batteryLevel[i] = 51;
+    }else if(splitArray[i] == 4){
+      batteryLevel[i] = 52;
+    }else if(splitArray[i] == 5){
+      batteryLevel[i] = 53;
+    }else if(splitArray[i] == 6){
+      batteryLevel[i] = 54;
+    }else if(splitArray[i] == 7){
+      batteryLevel[i] = 55;
+    }else if(splitArray[i] == 8){
+      batteryLevel[i] = 56;
+    }else if(splitArray[i] == 9){
+      batteryLevel[i] = 57;
+    }
+  }
+
+  
+  //write to terminal
+  for(int i=0; i<3; i++){
+    SerialBT.write(batteryLevel[i]);
+  }
 }
